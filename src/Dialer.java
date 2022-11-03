452ed5c3a4b78b1bced8976f992163b5dcb80a61
Joseph-Godwin-UWF <jcg55@students.uwf.edu>
2022-11-03 13:39:01 -0500

    More meaningful variable names

--- src/Dialer.java
+++ src/Dialer.java
@@ -1,12 +1,12 @@
-/**
- * This class is used for generating Brute-Force combinations
- * used to dail a safe with the following restrictions:
- *  - The Safe dail ranges from (0, 99]
- *  - The Safe uses a 3-number combo (XX, XX, XX)
- *
- * @Author Joseph Godwin
- * @Date November 2nd, 2022
- */
+/********************************************************************
+ * This class is used for generating Brute-Force combinations       *
+ * used to dail a safe with the following restrictions:             *
+ *  - The Safe dail ranges from (0, 99]                             *
+ *  - The Safe uses a 3-number combo (XX, XX, XX)                   *
+ *                                                                  *
+ * @Author Joseph Godwin                                            *
+ * @Date November 2nd, 2022                                         *
+ ********************************************************************/
 
 public class Dialer {
 
@@ -53,7 +53,7 @@ public class Dialer {
 
 
     /*******************************************************************
-     **  METHODS **
+     **  PUBLIC METHODS **
      *******************************************************************/
 
     /**
@@ -79,25 +79,25 @@ public class Dialer {
      * @return returns the next brute-force combination using the specified delta
      */
     public int[] getNextCombination(int delta){
-        int c1 = 0;
-        int c2 = 1;
-        int c3 = 2;
-        if(combination[c3] >= combination[c2] - delta && combination[c3] < combination[c2]
-           || combination[c3] >= 100 - delta && combination[c2] <= delta){
-            combination[c2] += delta;
-            if(combination[c2] >= 100){
-                combination[c2] -= 100;
-                combination[c1] += delta;
-                if(combination[c1] >= 100){
-                    combination[c1] -= 100;
+        int one = 0;
+        int two = 1;
+        int three = 2;
+        if(combination[three] >= combination[two] - delta && combination[three] < combination[two]
+           || combination[three] >= 100 - delta && combination[two] <= delta){
+            combination[two] += delta;
+            if(combination[two] >= 100){
+                combination[two] -= 100;
+                combination[one] += delta;
+                if(combination[one] >= 100){
+                    combination[one] -= 100;
                 }
             }
-            combination[c3] = combination[c2];
+            combination[three] = combination[two];
         }
         else{
-            combination[c3] += delta;
-            if(combination[c3] >= 100){
-                combination[c3] -= 100;
+            combination[three] += delta;
+            if(combination[three] >= 100){
+                combination[three] -= 100;
             }
         }
     return combination;
